c7132874dc9c94bd46d8424a60b3b35eb7bc75c2
Karolina.Vaittinen <karolinavaittinen@gmail.com>
2023-05-24 14:49:09 +0200

    Hotovo

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-console.log("funguju!");
+const formElm = document.querySelector("#formular");
+const nameElm = document.querySelector("#voter");
 
-fetch("https://apps.kodim.cz/daweb/hlasovani/api/poll/3", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    optionId: 2,
-    voterName: "Zuzka",
-  }),
-});
+const btnElm = document.querySelector(".btn");
+const hlasuj = (event) => {
+  event.preventDefault();
+
+  const mujHlas = { optionId: 0, voterName: nameElm.value };
+
+  fetch("https://apps.kodim.cz/daweb/hlasovani/api/poll/1", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(mujHlas),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "error") {
+        console.log("Nemůžete hlasovat 2x stejným jménem");
+      } else {
+        console.log("OK");
+      }
+    });
+};
+
+btnElm.addEventListener("click", hlasuj);
